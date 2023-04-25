c8d9683d5eee983ca61b2f486c4551f417b3f76e
Olga Bulat <obulat@gmail.com>
2023-04-25 06:46:20 +0300

    Update frontend/src/components/VHeader/VHeaderMobile/VContentSettingsButton.vue

--- frontend/src/components/VHeader/VHeaderMobile/VContentSettingsButton.vue
+++ frontend/src/components/VHeader/VHeaderMobile/VContentSettingsButton.vue
@@ -22,7 +22,7 @@
   </VSearchBarButton>
 </template>
 <script lang="ts">
-import VSearchBarButton from "../../../components/VHeader/VHeaderMobile/VSearchBarButton.vue"
+import VSearchBarButton from "~/components/VHeader/VHeaderMobile/VSearchBarButton.vue"
 
 import sourceIcon from "~/assets/icons/source.svg"
 
